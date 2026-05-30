47097110397ffa06c985860c3e389238df162ce9
Aya-Gaha <ayagaha1@gmail.com>
2026-05-30 15:38:38 +0100

    removing debugging functions from profile

--- src/modules/profile/profile.service.ts
+++ src/modules/profile/profile.service.ts
@@ -37,19 +37,10 @@ export class ProfileService {
         throw new NotFoundException('User not found');
       }
 
-      const existingProfile = await this.profileRepository.findOne({
-        where: { user: { id: userId } },
-      });
-
-      if (existingProfile) {
-        throw new BadRequestException(
-          'This user already has a profile. Use PUT /profile or the step update endpoints to edit it, or create Amine with a different user account.',
-        );
-      }
-
       // Progressive approach: Create profile with ONLY step1
       // Other steps will be added via PUT /profile/step/X
       const profileData_obj: any = {
+        
         bio: profileData.bio || this.generateDefaultBio(profileData.step1),
         userLevel: 'Junior', // Default level (will be updated in step2)
         skills: [],
@@ -462,5 +453,4 @@ if (!profile) {
       userLevel: profile.userLevel,
     };
   }
-
 }
